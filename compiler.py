5814ce726ac8c63f93b94a2f78d94b6071089b48
GiuPassarelli <gb_passarelli@hotmail.com>
2020-09-02 19:13:34 -0300

    Issue resolvida

--- compiler.py
+++ compiler.py
@@ -10,82 +10,90 @@ class Token:
 class Tokenizer:
     def __init__(self, origin):
         self.origin = origin
-        self.position = 1
+        self.position = 0
         self.actual = None
 
     def selectNext(self):
         pos = self.position
-        carac = self.origin[pos]
-        #print(self.origin[self.position])
-        if(carac.isdigit()):
-            pos += 1
-            while((self.origin[pos]).isdigit()):
-                carac += self.origin[pos]
-                #print(carac)
-                pos += 1
-            self.actual = Token("INT", int(carac))
-        
-        else:
-            if(carac == "+"):
+        if(len(self.origin)>pos):
+            carac = self.origin[pos]
+            if(carac.isdigit()):
+                while(len(self.origin)>(pos+1) and (self.origin[pos+1]).isdigit()):
+                    carac += self.origin[pos+1]
+                    pos += 1
+                self.actual = Token("INT", int(carac))
+
+            elif(carac == "+"):
                 self.actual = Token("PLUS", carac)
             elif(carac == "-"):
                 self.actual = Token("MINUS", carac)
-            elif(carac == "'"):
-                self.actual = Token("EOF", carac)
-            # else:
-            #     raise Exception("Simbolo nao indentificado")
-            pos += 1
+            #else:
+            #    raise Exception("Simbolo nao indentificado")
+
+        else:
+            self.actual = Token("EOF", "")
         
-        #print(pos)
+        pos += 1
         self.position = pos
 
 class Parser:
-    #tokens = 1
-
     @staticmethod
     def parseExpression():
         tokens = parser.tokens
-        tokens.selectNext()
         value = tokens.actual.value
         if(isinstance(value, int)):
             result = value
             tokens.selectNext()
             type_ = tokens.actual.type
+            if((type_ != "PLUS" and type_ != "MINUS") and parser.isNumberRepeated == True):
+                raise Exception("Números sem operação entre eles")
             while(type_ == "PLUS" or type_ == "MINUS"):
                 tokens.selectNext()
                 type2 = tokens.actual.type
                 if(type_ == "PLUS"):
+                    if(type2 != "INT" and type2 != "EOF"):
+                        tokens.selectNext()
+                        type2 = tokens.actual.type
                     if(type2 == "INT"):
                         result += tokens.actual.value
                     else:
                         raise Exception("Soma não seguida por número")
                 if(type_ == "MINUS"):
+                    if(type2 != "INT" and type2 != "EOF"):
+                        tokens.selectNext()
+                        type2 = tokens.actual.type
                     if(type2 == "INT"):
                         result -= tokens.actual.value
                     else:
                         raise Exception("Subtração não seguida por número")
                 tokens.selectNext()
                 type_ = tokens.actual.type
-            print(result)
+                tokens.actual.value = result
+                parser.isNumberRepeated = False
+
+            if(type_ != "EOF"):
+                parser.isNumberRepeated = True
+                parser.parseExpression()
+            else:
+                print(result)
         else:
             raise Exception("Não começa com número")
 
     @staticmethod
     def run(code):
         parser.tokens = Tokenizer(code)
+        parser.tokens.selectNext()
+        parser.isNumberRepeated = False
         parser.parseExpression()
 
-#print ('Argument:', sys.argv[1:])
+#print ('Argument:', sys.argv[1])
+arg = sys.argv[1]
+arg = " ".join(arg.split())
+
+#print("arg",arg)
 
 parser = Parser()
 
-arg = ""
-for i in sys.argv[1:]:
-    arg += i
-
-#print("ARG", arg)
-
-parser.run(arg)
-
+parser.run(arg.strip())
 
 #print(parser.tokens)
