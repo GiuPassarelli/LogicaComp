203bcaff42790ff9d8b82148403c2e0216bb1aae
GiuPassarelli <gb_passarelli@hotmail.com>
2020-09-09 01:26:01 -0300

    v1.1 done

--- compiler.py
+++ compiler.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 
+import re
 import sys
 
 class Token:
     def __init__(self, _type, value):
         self.type = _type
         self.value = value
+
+class PrePro:
+    @staticmethod
+    def filter(code):
+        clean = re.sub(r'#=[\D\d]*?=#', '', code)
+        clean = " ".join(clean.split())
+        return clean
 
 class Tokenizer:
     def __init__(self, origin):
@@ -14,6 +22,7 @@ class Tokenizer:
         self.actual = None
 
     def selectNext(self):
+        parser.blank = False
         pos = self.position
         if(len(self.origin)>pos):
             carac = self.origin[pos]
@@ -27,12 +36,18 @@ class Tokenizer:
                 self.actual = Token("PLUS", carac)
             elif(carac == "-"):
                 self.actual = Token("MINUS", carac)
-            #else:
-            #    raise Exception("Simbolo nao indentificado")
+            elif(carac == "*"):
+                self.actual = Token("MULTIPLY", carac)
+            elif(carac == "/"):
+                self.actual = Token("DIVIDE", carac)
+            elif(carac == " "):
+                parser.blank = True
+            elif(carac != " "):
+                raise Exception("Simbolo nao indentificado")
 
         else:
             self.actual = Token("EOF", "")
-        
+
         pos += 1
         self.position = pos
 
@@ -40,60 +55,79 @@ class Parser:
     @staticmethod
     def parseExpression():
         tokens = parser.tokens
-        value = tokens.actual.value
-        if(isinstance(value, int)):
-            result = value
+        result = parser.parseTerm()
+        type_ = tokens.actual.type
+        while(type_ == "PLUS" or type_ == "MINUS"):
+            term_result = parser.parseTerm()
+            if(type_ == "PLUS"):
+                result += term_result
+            if(type_ == "MINUS"):
+                result -= term_result
+            type_ = tokens.actual.type
+
+        print(result)
+
+    @staticmethod
+    def parseTerm():
+        tokens = parser.tokens
+        tokens.selectNext()
+        type1 = tokens.actual.type
+
+        #Resolve entradas tipo "1+ 2"
+        if((type1 == "PLUS" or type1 == "MINUS") and parser.blank):
+            tokens.selectNext()
+            type1 = tokens.actual.type
+        
+        if(type1 == "INT"):
+            result = tokens.actual.value
             tokens.selectNext()
             type_ = tokens.actual.type
-            if((type_ != "PLUS" and type_ != "MINUS") and parser.isNumberRepeated == True):
-                raise Exception("Números sem operação entre eles")
-            while(type_ == "PLUS" or type_ == "MINUS"):
-                tokens.selectNext()
-                type2 = tokens.actual.type
-                if(type_ == "PLUS"):
-                    if(type2 != "INT" and type2 != "EOF"):
-                        tokens.selectNext()
-                        type2 = tokens.actual.type
-                    if(type2 == "INT"):
-                        result += tokens.actual.value
-                    else:
-                        raise Exception("Soma não seguida por número")
-                if(type_ == "MINUS"):
-                    if(type2 != "INT" and type2 != "EOF"):
-                        tokens.selectNext()
-                        type2 = tokens.actual.type
-                    if(type2 == "INT"):
-                        result -= tokens.actual.value
-                    else:
-                        raise Exception("Subtração não seguida por número")
+
+            #Resolve entradas tipo "1 +2"
+            if(type_ == "INT"):
                 tokens.selectNext()
                 type_ = tokens.actual.type
-                tokens.actual.value = result
-                parser.isNumberRepeated = False
+                if(type_ == "INT"):
+                    raise Exception("Números sem operação entre eles")
 
-            if(type_ != "EOF"):
-                parser.isNumberRepeated = True
-                parser.parseExpression()
-            else:
-                print(result)
+            while(type_ == "MULTIPLY" or type_ == "DIVIDE"):
+                tokens.selectNext()
+                type2 = tokens.actual.type
+                if(type2 != "INT" and type2 != "EOF" and parser.blank):
+                    tokens.selectNext()
+                    type2 = tokens.actual.type
+                if(type_ == "MULTIPLY"):
+                    if(type2 == "INT"):
+                        result *= tokens.actual.value
+                    else:
+                        raise Exception("Multiplicação não seguida por número")
+                if(type_ == "DIVIDE"):
+                    if(type2 == "INT"):
+                        result /= tokens.actual.value
+                    else:
+                        raise Exception("Divisão não seguida por número")
+                tokens.selectNext()
+                type_ = tokens.actual.type
+                
+                #Resolve entradas tipo "1*2 *3"
+                if(type_ == "INT"):
+                    tokens.selectNext()
+                    type_ = tokens.actual.type
+
+            return(result)
         else:
-            raise Exception("Não começa com número")
+            raise Exception("Não começa ou não termina com número ou possui simbolos repetidos")
 
     @staticmethod
     def run(code):
-        parser.tokens = Tokenizer(code)
-        parser.tokens.selectNext()
-        parser.isNumberRepeated = False
+        clean = PrePro.filter(code)
+        parser.tokens = Tokenizer(clean)
         parser.parseExpression()
 
-#print ('Argument:', sys.argv[1])
-arg = sys.argv[1]
-arg = " ".join(arg.split())
-
-#print("arg",arg)
-
 parser = Parser()
+parser.run(sys.argv[1])
 
-parser.run(arg.strip())
 
-#print(parser.tokens)
+#PROBLEMAS: 
+# "2++3"
+# Chamada de selectNext antes do term (no run)
